2bda8d00b1cae0133ef8f8e275729f56c10e8dbd
tarun-kataruka <tarunkataruka22@gmail.com>
2025-04-13 16:58:55 +0530

    refactor: Update file paths for improved organization

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 
 var images = [
-        'url("/images/pic1.jpg")',
-        'url("/images/pic2.jpg")',
-        'url("/images/pic3.jpg")',
-        'url("/images/pic4.jpg")',
-        'url("/images/pic5.jpg")',
-        'url("/images/pic6.jpg")',
+        'url("images/pic1.jpg")',
+        'url("images/pic2.jpg")',
+        'url("images/pic3.jpg")',
+        'url("images/pic4.jpg")',
+        'url("images/pic5.jpg")',
+        'url("images/pic6.jpg")',
     ]
     function changeBackground(){
     const bg= images[Math.floor(Math.random()*images.length)];
